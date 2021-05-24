655e7d60a7e157a3c23ad5d1a086e3d295f3ad09
jagoulartpuc <jose.goulart@acad.pucrs.br>
2021-05-24 18:28:25 -0300

    done

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const kcAdminClient = require('./keycloakClient.js');
 const app = express();
 const port = 8083;
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
-
-var credentials = {
+const credentials = {
   username: 'admin',
   password: 'admin',
   grantType: 'password',
-  clientId: 'account',
-  clientSecret: '4c34747a-10c2-4877-9a98-d3d51f41b9a7'
+  clientId: 'master-realm',
+  totp: '123456',
+  clientSecret: '02a81ca7-2433-4c30-ae7c-2e9ea3030543',
 };
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
 
 app.get('/auth', async (req, res) => {
 
@@ -31,7 +37,6 @@ app.get('/auth', async (req, res) => {
   }
 });
 
-// Returns all users from keycloak
 app.get('/users', async (req, res) => {
   let users;
   try {
@@ -40,11 +45,10 @@ app.get('/users', async (req, res) => {
     console.log(err);
     res.status(500).send(err);
   }
-  
+
   res.send(users);
 });
 
-// Returns an user by id
 app.get('/users/:id', async (req, res, _) => {
   let user;
   try {
@@ -55,15 +59,9 @@ app.get('/users/:id', async (req, res, _) => {
   }
 
   res.status(200).send(user);
-  
+
 });
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true,
-}));
-
-// Creates user
 app.post('/users', async (req, res) => {
   var { username, email, firstName, lastName } = req.body;
   let response;
@@ -84,7 +82,6 @@ app.post('/users', async (req, res) => {
   res.status(200).send(response);
 });
 
-// Updates the user data
 app.put('/users/:id', async (req, res) => {
   const { firstName, lastName } = req.body;
   let response;
@@ -106,17 +103,13 @@ app.put('/users/:id', async (req, res) => {
   res.status(200).send(response);
 });
 
-// Updates the user password
 app.patch('/users/:id', async (req, res) => {
-  const { password } = req.body;
-  const { id } = req.params;
+  const password = req.params;
+  const id = req.params;
   let response;
   try {
     response = await kcAdminClient.users.update(
-      { id },
-      {
-        password,
-      }
+      id, password
     );
   } catch (err) {
     console.log(err);
@@ -126,15 +119,13 @@ app.patch('/users/:id', async (req, res) => {
   res.status(200).send(response);
 });
 
-// Deletes an user by id
 app.delete('/users/:id', async (req, res) => {
-  let response;
   try {
-    response = await kcAdminClient.users.del({ id: req.params.id });
+    await kcAdminClient.users.del({ id: req.params.id });
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
   }
 
-  res.status(200).send(response);
+  res.status(200).send(true);
 });
--- keycloakClient.js
+++ keycloakClient.js
@@ -3,7 +3,7 @@ const KcAdminClient = require('keycloak-admin').default;
 const kcAdminClient = new KcAdminClient();
 
 kcAdminClient.setConfig({
-    baseUrl: 'http://18.221.148.148:8080/auth',
+    baseUrl: 'http://3.15.140.59:8080/auth',
     realmName: 'master'
 });
 
